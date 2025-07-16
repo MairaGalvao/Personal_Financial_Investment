240bfc03b309b2e6e032f354b842399102c392fb
MairaGalvao <mairagalvao01@gmail.com>
2025-07-16 16:03:27 +0300

    Add .env to .gitignore

--- etl.py
+++ etl.py
@@ -1,5 +1,7 @@
-from load_couple_expenses_data import LoadCoupleExpensesData
+from dotenv import load_dotenv
+import os
 import pandas as pd
+from load_couple_expenses_data import LoadCoupleExpensesData
 
 class CoupleDataETL:
 
@@ -7,8 +9,7 @@ class CoupleDataETL:
         self.couple_expenses_data = LoadCoupleExpensesData()
 
     def load_csv(self):
-        data = self.couple_expenses_data.load_csv()
-        return data
+        return self.couple_expenses_data.load_csv()
 
     def calculate_couple_csv_data(self, df):
         df['date'] = pd.to_datetime(df['date'])
@@ -26,13 +27,25 @@ class CoupleDataETL:
             'income': income,
             'outcome': outcome
         }).fillna(0).reset_index()
-
         summary['net'] = summary['income'] - summary['outcome']
         return summary
 
 if __name__ == "__main__":
+    load_dotenv()
+
+    OUTPUT_SUMMARY_FILE = os.getenv('OUTPUT_SUMMARY_FILE')
+    
     etl = CoupleDataETL()
     df = etl.load_csv()
-    df = etl.calculate_couple_csv_data(df)
-    summary_2024 = etl.monthly_summary_2024(df)
-    print(summary_2024)
+    if not df.empty:
+        df = etl.calculate_couple_csv_data(df)
+        summary_2024 = etl.monthly_summary_2024(df)
+        print(summary_2024)
+
+        if OUTPUT_SUMMARY_FILE:
+            summary_2024.to_csv(OUTPUT_SUMMARY_FILE, index=False)
+            print(f"✅ Summary saved to {OUTPUT_SUMMARY_FILE}")
+        else:
+            print("⚠️ OUTPUT_SUMMARY_FILE not set in .env")
+    else:
+        print("⚠️ Failed to load data or empty CSV.")
