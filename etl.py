e8c4bb88941cdf918c87a6613bbc3e7c4058ecce
MairaGalvao <mairagalvao01@gmail.com>
2025-07-16 15:57:30 +0300

    feat: add monthly summary calculation for couple expenses per year

--- etl.py
+++ etl.py
@@ -1,4 +1,5 @@
 from load_couple_expenses_data import LoadCoupleExpensesData
+import pandas as pd
 
 class CoupleDataETL:
 
@@ -6,9 +7,32 @@ class CoupleDataETL:
         self.couple_expenses_data = LoadCoupleExpensesData()
 
     def load_csv(self):
-        return self.couple_expenses_data.load_csv()
+        data = self.couple_expenses_data.load_csv()
+        return data
+
+    def calculate_couple_csv_data(self, df):
+        df['date'] = pd.to_datetime(df['date'])
+        df['year'] = df['date'].dt.year
+        df['month'] = df['date'].dt.month
+        return df
+
+    def monthly_summary_2024(self, df):
+        df_2024 = df[df['year'] == 2024].copy()
+        income_mask = df_2024['category'].str.lower() == 'income'
+        outcome_mask = df_2024['amount (₪)'] < 0
+        income = df_2024[income_mask].groupby('month')['amount (₪)'].sum()
+        outcome = df_2024[outcome_mask].groupby('month')['amount (₪)'].sum().abs()
+        summary = pd.DataFrame({
+            'income': income,
+            'outcome': outcome
+        }).fillna(0).reset_index()
+
+        summary['net'] = summary['income'] - summary['outcome']
+        return summary
 
 if __name__ == "__main__":
     etl = CoupleDataETL()
     df = etl.load_csv()
-    print(df.head())
+    df = etl.calculate_couple_csv_data(df)
+    summary_2024 = etl.monthly_summary_2024(df)
+    print(summary_2024)
--- load_couple_expenses_data.py
+++ load_couple_expenses_data.py
@@ -2,13 +2,11 @@ import logging
 import psutil
 import os
 import pandas as pd
-from dotenv import load_dotenv
 
 class LoadCoupleExpensesData:
     
     def load_csv(self):
-        load_dotenv()
-        self.data_file = os.getenv("DATA_FILE", "./data/financial_data_2024_2025.csv") 
+        self.data_file = os.getenv("DATA_FILE", "./data/financial_data_couple_shekels.csv") 
         print(f"Loading data from {self.data_file} ...")
         df = pd.read_csv(self.data_file)
         print(f"Loaded {len(df)} rows.")
